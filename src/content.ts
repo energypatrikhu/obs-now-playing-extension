fbde5a37bb064ce29fbd65a344e52c3bc7b0444e
EnergyPatrikHU <energypatrikhu@proton.me>
2024-05-07 14:02:33 +0200

    Add try ... catch

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,5 @@
 import { locationObserver } from './libs/locationObserver';
+import { sleep } from '@energypatrikhu/node-core-utils';
 
 const apiUrl = 'http://127.0.0.1:2442/api/nowPlaying';
 
@@ -9,45 +10,61 @@ let [_vid, __vid, vid]: [string | null, string | null, string | null] = [
 ];
 let isChanged = false;
 
-async function postToApi(videoId: string, time: number) {
-	await fetch(apiUrl, {
-		method: 'POST',
-		body: JSON.stringify({ videoId, time }),
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	});
+async function postToApi(videoId: string, time: number, retries = 0) {
+	try {
+		await fetch(apiUrl, {
+			method: 'POST',
+			body: JSON.stringify({ videoId, time }),
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+	} catch (error) {
+		console.error('Error while sending data to server', error);
+
+		if (retries < 3) {
+			console.log(
+				`Retrying to send data to server after ${retries + 1} second`,
+			);
+			await sleep(1000 * retries + 1);
+			await postToApi(videoId, time, retries + 1);
+		}
+	}
 }
 
 locationObserver(async (href) => {
-	if (!href.includes('watch')) {
-		return;
+	try {
+		if (!href.includes('watch')) {
+			return;
+		}
+
+		if (isChanged) {
+			return;
+		}
+		isChanged = true;
+
+		const searchParams = new URLSearchParams(window.location.search);
+		vid = searchParams.get('v');
+
+		if (vid === null) {
+			return;
+		}
+
+		if (vid === _vid) {
+			return;
+		}
+
+		_vid = vid;
+
+		console.log(`VideoID changed from '${__vid}' to '${vid}'`);
+		console.log('Data changed, sending new data to server');
+
+		await postToApi(vid, Date.now());
+
+		__vid = vid;
+
+		isChanged = false;
+	} catch (error) {
+		console.error('Error in locationObserver', error);
 	}
-
-	if (isChanged) {
-		return;
-	}
-	isChanged = true;
-
-	const searchParams = new URLSearchParams(window.location.search);
-	vid = searchParams.get('v');
-
-	if (vid === null) {
-		return;
-	}
-
-	if (vid === _vid) {
-		return;
-	}
-
-	_vid = vid;
-
-	console.log(`VideoID changed from '${__vid}' to '${vid}'`);
-	console.log('Data changed, sending new data to server');
-
-	await postToApi(vid, Date.now());
-
-	__vid = vid;
-
-	isChanged = false;
 });
